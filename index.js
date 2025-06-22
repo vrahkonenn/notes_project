a7c712bb7b53941699255f87e4ff56cd8ba9d1e0
vrahkonenn <vrahkonen02@gmail.com>
2025-06-22 21:22:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 
 app.use(express.json())
 app.use(express.static('dist'))
-app.use(cors())
 
 let notes = [
   {
