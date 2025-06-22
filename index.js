3386075c86aaa249dff571f92e1a221627871cef
vrahkonenn <vrahkonen02@gmail.com>
2025-06-22 18:37:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
