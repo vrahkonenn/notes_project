c15cb06fb9032290829ae048a9090798bd236f99
vrahkonenn <vrahkonen02@gmail.com>
2025-07-02 20:46:06 +0300

    Linttaus

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const Note = require('./models/note')
+const config = require('./utils/config')
 
 const app = express()
 const cors = require('cors')
@@ -11,28 +12,28 @@ app.use(cors())
 
 
 app.post('/api/notes', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-    })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
     .catch(error => next(error))
 })
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello world!</h1>')
+  response.send('<h1>Hello world!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        console.log(notes)
-        response.json(notes)
-    })
+  Note.find({}).then(notes => {
+    console.log(notes)
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
@@ -40,7 +41,7 @@ app.get('/api/notes/:id', (request, response, next) => {
     .findById(request.params.id)
     .then(note => {
       if (note) {
-      response.json(note)
+        response.json(note)
       } else {
         response.status(404).end()
       }
@@ -49,7 +50,7 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const {content, important} = request.body
+  const { content, important } = request.body
 
   Note
     .findById(request.params.id)
@@ -69,12 +70,12 @@ app.put('/api/notes/:id', (request, response, next) => {
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-    Note
-      .findByIdAndDelete(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
+  Note
+    .findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -89,7 +90,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message})
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -97,7 +98,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const port = process.env.PORT
+const port = config.PORT
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
